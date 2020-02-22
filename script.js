f4324ee2a6753cf275f980328499ac5aab31cb82
Willian Inocencio <willian.i.alves@hotmail.com>
2020-02-22 16:57:45 -0300

    Add Image Entity to Canavas

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var ctx;
 var canvasWidth;
 var canvasHeight;
 var id;
+var entityImage = new Image();
 
 function InitTests() {
   canvas = document.getElementById('canvas');
@@ -13,27 +14,35 @@ function InitTests() {
   canvasHeight = 100;
   id = ctx.getImageData(0, 0, 1, 1);
   ctx.scale(5,5);
+  entityImage.src = 'pizza.png';
 }
 
 function FillRectTest() {
   var cycles = document.getElementById('cycles');
 
   ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-
-  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
   for (var i = 0; i < cycles.value; ++i) {
     x = Math.floor(Math.random() * canvasWidth);
     y = Math.floor(Math.random() * canvasHeight);
-
-    ctx.fillStyle = `rgb(0,0,0)`;
-    ctx.fillRect(x, y, 1, 1);
+    r = 180;
+    addEntityImage(x,y,r);
   }
+  
+}
+
+function addEntityImage(x,y,r) {
+  rotateEntityImage(r);
+  ctx.drawImage(entityImage, x, y, 5, 5);
 }
 
 function DownloadCanvas(){
-    var download = document.getElementById("downloadCanvas");
-    var image = document.getElementById("canvas").toDataURL("image/png")
-        .replace("image/png", "image/octet-stream");
-    download.setAttribute("href", image);
+  var download = document.getElementById("downloadCanvas");
+  var image = document.getElementById("canvas").toDataURL("image/png")
+  .replace("image/png", "image/octet-stream");
+  download.setAttribute("href", image);
+}
+
+function rotateEntityImage(r){
+  //entityImage.style.transform = "rotate("+r+"deg)";
 }
 
